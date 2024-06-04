452e82c2b68b25ec49c9875c7b1129824f57c13e
John Nguyen <john12052003@gmail.com>
2024-06-04 22:23:43 +0700

    Update Annoucement.jsx

--- src/page/Annoucement.jsx
+++ src/page/Annoucement.jsx
@@ -36,7 +36,7 @@ const Annoucement = ({ containerData }) => {
   return (
     <section className="w-screen h-screen airbnb pt-0 lg:pt-[100px] bg-sky-100 px-5 lg:px-[150px] pb-[50px] airbnb">
       
-      <div className="w-full h-full flex flex-col lg:flex-row gap-5">
+      <div className="w-full h-auto lg:h-full flex flex-col lg:flex-row gap-5">
         <div className="w-full lg:w-[60%] h-full flex flex-col gap-3">
           <div className="w-full h-[200px] flex flex-row items-end justify-between">
             <h2 className="text-start font-semibold text-black text-base lg:text-3xl ">
@@ -50,26 +50,26 @@ const Annoucement = ({ containerData }) => {
               <ArrowRightAltRounded />
             </HashLink>
           </div>
-          <div className="w-full h-full relative">
-            <div className="absolute top-2 lg:top-10 left-10 flex flex-col gap-1 ">
+          <div className="w-full h-[220px] lg:h-full relative ">
+            <div className="absolute top-2 lg:top-10 left-2 lg:left-10 flex flex-col gap-1 ">
               <div className=" bg-white text-black text-xs lg:text-base w-auto h-auto rounded-3xl px-2 lg:px-5 py-2 lg:py-3">
                 {reversedContainers[0].date}
               </div>
               <div className="bg-transparent backdrop-blur-lg max-w-[120px] w-auto flex flex-row justify-center items-center h-auto border-white gap-3 rounded-3xl px-3 py-2">
                 <Circle style={{ fontSize: 10, color: "white" }} />
-                <h2 className="text-white text-sm">
+                <h2 className="text-white text-xs lg:text-sm">
                   {reversedContainers[0].category}
                 </h2>
               </div>
             </div>
-            <div className="absolute top-20 bg-white/70 backdrop-blur-lg py-4 rounded-2xl lg:top-10 right-0 lg:right-10 px-5 text-base lg:text-3xl font-bold w-full lg:w-[50%] leading-[3rem] ">
-              <span className="text-blacktext-center">
+            <div className="absolute h-auto lg:h-[200px]  bottom-12  bg-white/70 backdrop-blur-lg py-1 px-4 mr-2 lg:py-4 rounded-2xl lg:top-10 right-0 lg:right-10 lg:px-5 text-sm  lg:text-3xl font-bold w-[70%] lg:w-[50%] leading-[1.2rem] lg:leading-[3rem] ">
+              <h5 className="text-black text-start h-auto">
                 {reversedContainers[0].heading}
-              </span>
+              </h5>
             </div>
             <HashLink
               to={`/ViewAllPost/${reversedContainers.length - 1}#top`}
-              className="absolute bottom-10 right-10 w-[70px] h-[70px] hover:scale-125 border-white hover:border-2 transition-all duration-200 rounded-full bg-white flex justify-center items-center"
+              className="absolute  bottom-1 lg:bottom-10 right-2 lg:right-10 w-[40px] h-[40px] lg:w-[70px] lg:h-[70px] hover:scale-125 border-white hover:border-2 transition-all duration-200 rounded-full bg-white flex justify-center items-center"
             >
               <ArrowOutwardTwoTone  style={{ fontSize: isLargeScreen ? 60 : 30, color: "black" }}  />
             </HashLink>
@@ -86,23 +86,23 @@ const Annoucement = ({ containerData }) => {
           </div>
         </div>
 
-        <div className="w-[40%] h-full flex flex-col gap-7">
-          <div className="w-full h-2/3 rounded-3xl bg-gray-400 relative">
-            <div className="absolute top-2 left-10 flex flex-col gap-4 ">
+        <div className="w-full lg:w-[40%] h-auto lg:h-full flex flex-col gap-7">
+          <div className="w-full h-[220px] lg:h-2/3 rounded-3xl bg-gray-400 relative">
+            <div className="absolute top-2 left-2 lg:left-10 flex flex-col gap-4 ">
               <div className="bg-transparent backdrop-blur-lg max-w-[150px] w-auto flex flex-row justify-center items-center h-auto border-white gap-3 rounded-3xl px-3 py-2">
                 <Circle style={{ fontSize: 10, color: "white" }} />
-                <h2 className="text-white text-sm">
+                <h2 className="text-white text-xs lg:text-sm">
                   {reversedContainers[0].category}
                 </h2>
               </div>
             </div>
 
-            <div className="absolute top-12 left-6 bg-white/60 backdrop-blur-lg py-2 rounded-2xl px-5 text-xl font-bold w-[70%] leading-[2rem]">
-              <span className="text-black text-center">
+            <div className="absolute h-auto lg:h-[120px] bottom-12 lg:top-12 right-2 lg:left-6 bg-white/60 backdrop-blur-lg py-1 lg:py-2 rounded-2xl px-4 lg:px-5 text-xs lg:text-xl font-bold w-[70%] leading-[1.2rem] lg:leading-[2rem]">
+              <div className="text-black text-start h-auto">
                 {reversedContainers[1].heading}
-              </span>
+              </div>
             </div>
-            <div className="absolute top-[14rem] left-10 py-2 px-2 text-sm font-bold w-[80%] text-white bg-black/40 backdrop-blur-lg rounded-3xl ">
+            <div className="hidden lg:block absolute top-[14rem] left-10 py-2 px-2 text-sm font-bold w-[80%] text-white bg-black/40 backdrop-blur-lg rounded-3xl ">
               {transform(reversedContainers[1].p)}
             </div>
             <div className="absolute top-2 right-2 py-2 px-2 font-bold w-auto h-auto rounded-full backdrop-blur-md bg-white/40 ">
@@ -124,7 +124,7 @@ const Annoucement = ({ containerData }) => {
               alt=""
             ></img>
           </div>
-          <div className="w-full h-1/2 rounded-3xl bg-gray-400 flex relative justify-center items-end">
+          <div className="hidden lg:flex lw-full h-1/2 rounded-3xl bg-gray-400  relative justify-center items-end">
             <img
               className="w-full h-full object-cover rounded-3xl absolute top-0 left-0"
               src={
